7665de4ecc17a179dfa59f19abe39adc219d0604
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-07-29 00:31:04 +0300

    Creating database using python

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+# Replace with your connection details
+
+try:
+    with mysql.connector.connect(
+        host="localhost",
+        user="Abdelrahman",
+        password="123456",
+        database="alx_book_store",
+    ) as connection:
+        create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+        with connection.cursor() as cursor:
+            cursor.execute(create_db_query)
+        print("Database 'alx_book_store' created successfully!")
+except mysql.connectorconnect as e:
+    print("Failed to connect to the database!")
